38a7eda0d65a1384db7d3cb3248bd56d87b926ba
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-29 14:17:31 +0530

    Created the model

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
-    var model = {};
+$(document).ready(function () {
+    var model = {
+        currentCat: null,
+        cats: [{
+            name: "Minie",
+            imgURL: "https://kittenrescue.org/wp-content/uploads/2017/03/KittenRescue_KittenCareHandbook.jpg",
+            count: 0
+        }, {
+            name: "Cutie",
+            imgURL: "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
+            count: 0
+        }, {
+            name: "Tomin",
+            imgURL: "http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg",
+            count: 0
+        }, {
+            name: "Lucy",
+            imgURL: "http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-4-57b30a939dff5__605.jpg",
+            count: 0
+        }, {
+            name: "Mia",
+            imgURL: "https://i.pinimg.com/736x/16/9e/fd/169efd8c7bb1f9504335171340dcce0a--fluffy-kittens-baby-kittens.jpg",
+            count: 0
+        }]
+    };
 
     var octopus = {
         init: function () {
